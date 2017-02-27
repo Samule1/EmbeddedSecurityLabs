9f7871fbf5d02a4b544b8f5619059285b6481b13
Flarpz <Marcuskarrm@gmail.com>
2017-02-27 15:58:01 +0100

    Updated ex 4

--- Ex4/Ex4/src/XORInputStream.java
+++ Ex4/Ex4/src/XORInputStream.java
@@ -7,56 +7,52 @@ public class XORInputStream extends InputStream {
 	private String encrypted;
 	private String key;
 	private char letter;
-	
+
 	public XORInputStream(String k, String text){
 		plainText = text;
 		key  = k;
 	}
-	
+
 	@Override
 	public int read() throws IOException {
 		int nr = (int)letter;
 		return nr;
 	}
-	
+
 	public char getChar() {
 		return this.letter;
 	}
-	
+
 	public void setChar(char val) {
 		letter = val;
 	}
-	
-	
+
+
 	public String GetEncryptedMsg(){
 		return encrypted;
 	}
-	
+
 	public void SetEncryptedMsg(String encMsg){
 		encrypted = encMsg;
 	}
-	
+
 	public void XOREncryption() throws IOException{
 		String encryptedMsg = "";
-		int textInt[] = new int[plainText.length()]; 
+		int textInt[] = new int[plainText.length()];
 		int keyInt[] = new int[plainText.length()];
 		for(int i=0; i<plainText.length(); i++){
-			if(Character.isLowerCase(plainText.charAt(i))){
-				System.out.println("Not only Uppercase");
-			}
-			else{
 				setChar(plainText.charAt(i));
 				textInt[i] = read();
 				setChar(key.charAt(i));
 				keyInt[i] = read();
-			}
+
 		}
 		for(int i=0; i<textInt.length; i++){
 			encryptedMsg += (char)(textInt[i]^ keyInt[i]);
 		}
 		SetEncryptedMsg(encryptedMsg);
 	}
-	
+
 	public String XORDecryption() throws IOException{
 		String decryptedMsg = "";
 		int textInt[] = new int[plainText.length()];
@@ -72,20 +68,25 @@ public class XORInputStream extends InputStream {
 		}
 		return decryptedMsg;
 	}
-	
-	
+
+
 	public static void main(String args[]) throws IOException{
-		XORInputStream test = new XORInputStream("GKAICC", "MARCUS");
-		
+		XORInputStream test = new XORInputStream("dsiMMs", "MARCUS");
+
 		test.XOREncryption();
-		System.out.println(test.GetEncryptedMsg());
-		System.out.println(test.XORDecryption());
-		
+		System.out.println("Encrypted msg : " + test.GetEncryptedMsg());
+		System.out.println("Decrypted msg : " + test.XORDecryption());
+
 		XORInputStream test2 = new XORInputStream("DGSDTS", "HAMPUS");
 		test2.XOREncryption();
-		System.out.println(test2.GetEncryptedMsg());
-		System.out.println(test2.XORDecryption());
+		System.out.println("Encrypted msg : " + test2.GetEncryptedMsg());
+		System.out.println("Decrypted msg : " +test2.XORDecryption());
+
+		XORInputStream test3 = new XORInputStream("KG12cb", "Marc11");
+		test3.XOREncryption();
+		System.out.println("Encrypted msg : " + test3.GetEncryptedMsg());
+		System.out.println("Decrypted msg : " + test3.XORDecryption());
 	}
-	
+
 
 }
